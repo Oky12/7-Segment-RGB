f26073ed5000e63ef1279f700378d5ec5daa55da
Oky Suryanto <30838114+Oky12@users.noreply.github.com>
2018-12-03 21:54:07 +0700

    Update Digital7SegRGB.h

--- Digital7SegRGB.h
+++ Digital7SegRGB.h
@@ -3,13 +3,15 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 
-class Digital7SegRGB{
-	public:
-	  void begin(int Pin, int Digit);
-	  void Clear();
-	  void SetSeg(int Seg, int R, int G, int B);
-	  void PrintDigit(int Value, int Digit, int R, int G, int B);
+class Digital7SegRGB {
+public:
+	void begin(int Pin, int Digit);
+	void Clear();
+	void SetSeg(int Seg, int R, int G, int B);
+	void PrintDigit(int Value, int Digit, int R, int G, int B);
 private:
 	int TempValue;
+	int PIXEL;
+	int pin;
 };
 #endif
